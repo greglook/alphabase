ae8494fee63b9cadd784c5a26e3cc10c4028de16
Greg Look <greg@greg-look.net>
2016-03-07 12:06:50 -0800

    Set minor version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/alphabase "0.2.0-SNAPSHOT"
+(defproject mvxcvi/alphabase "0.2.0"
   :description "Clojure(script) library to encode binary data with alphabet base strings."
   :url "https://github.com/greglook/alphabase"
   :license {:name "Public Domain"
