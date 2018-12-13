fb081f3f91ff77e5ac77e851bd1b77a404762f91
Greg Look <greg@greg-look.net>
2018-12-12 19:15:07 -0800

    Set patch version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/alphabase "2.0.1-SNAPSHOT"
+(defproject mvxcvi/alphabase "2.0.1"
   :description "Clojure(script) library to encode binary data with alphabet base strings."
   :url "https://github.com/greglook/alphabase"
   :license {:name "Public Domain"
